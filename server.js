69fd97e02cf24511139d0035bd7477ac179a95ca
Glitch (eight-shift) <none>
2018-12-16 16:34:22 +0000

    🍺🕴 Checkpoint ./server.js:2505047/603 ./package.json:2505047/1303 ./uploads/cool-file.js:2505047/11

--- server.js
+++ server.js
@@ -2,6 +2,17 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer')
+var upload = multer({ dest: '/uploads/' })
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username:{type:String,required:true},
+}); 
 
 // require and use "multer"...
 
@@ -13,6 +24,12 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
+
+app.post('/profile', upload.single('avatar'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+})
+
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
